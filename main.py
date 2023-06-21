f3c23de1c8a58da75d8583364e77c0b440022c98
= <=>
2023-06-21 12:34:56 -0300

    Adicionado a funcionalidade de também sincronizar a hora

--- main.py
+++ main.py
@@ -8,39 +8,102 @@ import requests
 from datetime import datetime
 import os
 
-def time_API():
+# Dados da internet
+class API():
     url = 'http://worldtimeapi.org/api/timezone/America/Sao_Paulo'
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        hora = data['datetime']
+    def time(self):
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                dados = response.json()
+                hora = dados['datetime']
+                return hora[11:16]
+
+            else:
+                print('Não foi possível obter o horário da internet.')
+                exit()
+        except Exception as erro:
+            print('Erro, não foi possível conectar-se a internet.'+str(erro))
+            exit()
+
+    def date(self):
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                dados = response.json()
+                data = dados['datetime']
+                return data[:10]
+
+            else:
+                print('Erro, Não foi possível obter a data da internet.')
+                exit()
+        except Exception as erro:
+            print('Erro, não foi possível conectar-se a internet.'+str(erro))
+            exit()
+
+# dados do computador
+class computer():
+    dados = datetime.now()
+    def time(self):
+        hora= str(self.dados)
         return hora[11:16]
-    else:
-        print('Não foi possível obter o horário da internet.')
-        exit()
 
-def time_system():
-    hora = str(datetime.now())
-    return hora[11:16]
+    def date(self):
+        data = str(self.dados)
+        return data[:10]
 
-def time_sinc():
-    cmd = 'time ' +time_API()
+computador = computer()
+internet = API()
+
+# funções para sincronizar
+def timesinc():
+    nova_hora = API()
+    cmd = 'time '+nova_hora.time()
     os.system(cmd)
-    print('Horário definido para ', time_API())
+    print('Horário alterado com sucesso!')
+    exit()
 
-hora_internet = time_API()
-hora_system = time_system()
-while True:
-    if hora_system != hora_internet:
-        print(f'Hora atual do sistema: {hora_system}\nHora atual da internet: {hora_internet}.')
-        print('A hora do seu computador não está correta. quer corrigir?')
-        question = input('S/sim, N/não')
-        if question.upper() == 'S':
-            time_sinc()
-            break
-        elif question.upper() == 'N':
-            print('Saindo...')
-            break
-    else:
-        print('A hora do seu sistema está sincronizada com a internet, você não precisa fazer nada')
-        break
+def datesinc():
+    dados = API()
+    date_split = dados.date().split('-')
+    new_date = '{}/{}/{}'.format(date_split[2], date_split[1], date_split[0])
+    os.system('date '+new_date)
+    print('Data alterada com sucesso!'+new_date)
+    step_time()
+
+loop = True
+def step_time():
+    loop = False
+    while True:
+        if internet.time() != computador.time():
+            print(f'Hora da internet: {internet.time()}.\nHora do sistema: {computador.time()}.')
+            option_time = input('O horário precisa ser ajustado, deseja fazer isso agora?\tS/sim, N/não.')
+            if option_time.upper() == 'N':
+                print('Saindo sem ajustar a hora...')
+                break
+            elif option_time.upper() == 'S':
+                timesinc()
+                break
+
+        else:
+            print('A hora não precisa ser ajustada, fique tranquilo!')
+            exit()
+
+def step_date():
+    while loop!=False:
+        if internet.date() != computador.date():
+            print(f'Data do sistema: {computador.date()}.\nData da internet: {internet.date()}.')
+            option_date = input('A data precisa ser ajustada, deseja fazer isso agora?\tS/sim, N/não.')
+            if option_date.upper() == 'N':
+                print('Saindo sem ajustar a data...')
+                step_time()
+            elif option_date.upper() == 'S':
+                datesinc()
+
+        else:
+            print('A data não precisa ser ajustada, fique tranquilo!')
+            step_time()
+
+def main():
+    step_date()
+main()
